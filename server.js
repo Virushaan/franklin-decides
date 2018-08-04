467e9ca0fc95d40ee08b454f44d9451dbc5e6ef1
Virushaan <virusedmmusic@gmail.com>
2018-08-05 03:50:23 +1000

    https fix

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 //Install express server
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
-const forceHTTPS = require("expressjs-force-https").forceHTTPS;
+const path = require("path");
+
 const app = express();
 
-app.use(forceHTTPS);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/franklin-decides"));
 
-// Serve only the static files frmm the dist directory
-app.use(express.static(__dirname + '/dist/franklin-decides'));
+app.use(function(req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+});
 
-app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/franklin-decides/index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/dist/franklin-decides/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
